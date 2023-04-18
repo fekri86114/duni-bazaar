8abffe625c94b436633bfa5e97c4141fc4deae18
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-04-18 07:50:12 -0700

    added `SignUpScreen.kt` view

--- app/src/main/java/info/fekri/dunibazaar/ui/features/signUp/SignUpScreen.kt
+++ app/src/main/java/info/fekri/dunibazaar/ui/features/signUp/SignUpScreen.kt
@@ -1,20 +1,27 @@
 package info.fekri.dunibazaar.ui.features.signUp
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import info.fekri.dunibazaar.R
 import info.fekri.dunibazaar.ui.features.IntroScreen
 import info.fekri.dunibazaar.ui.theme.BackgroundMain
+import info.fekri.dunibazaar.ui.theme.Blue
 import info.fekri.dunibazaar.ui.theme.MainAppTheme
 import info.fekri.dunibazaar.ui.theme.Shapes
 
@@ -33,6 +40,125 @@ fun SignUpPreview() {
 
 @Composable
 fun SignUpScreen() {
+
+    Box {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.4f)
+                .background(Blue)
+        )
+        
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.95f),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            IconApp()
+
+            MainCardView {
+
+            }
+        }
+        
+    }
+
+}
+
+@Composable
+fun IconApp() {
+    Surface(
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(64.dp)
+    ) {
+       Image(
+           modifier = Modifier.padding(14.dp),
+           painter = painterResource(id = R.drawable.ic_icon_app),
+           contentDescription = null
+       )
+    }
+}
+
+@Composable
+fun MainCardView(signUpEvent: () -> Unit) {
+    val name = remember { mutableStateOf("null") }
+    val email = remember { mutableStateOf("null") }
+    val password = remember { mutableStateOf("null") }
+    val confirmPassword = remember { mutableStateOf("null") }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        elevation = 10.dp,
+        shape = Shapes.medium
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                modifier = Modifier.padding(top = 18.dp, bottom = 18.dp),
+                text = "Sign Up",
+                style = TextStyle(color = Blue, fontSize = 28.sp, fontWeight = FontWeight.Bold)
+            )
+
+            MainTextField(
+                edtValue = name.value,
+                icon = R.drawable.ic_person,
+                hint = "Your full name"
+            ) { name.value = it }
+
+            MainTextField(
+                edtValue = email.value,
+                icon = R.drawable.ic_email,
+                hint = "Email"
+            ) { email.value = it }
+
+            MainTextField(
+                edtValue = password.value,
+                icon = R.drawable.ic_password,
+                hint = "Password"
+            ) { password.value = it }
+
+            MainTextField(
+                edtValue = confirmPassword.value,
+                icon = R.drawable.ic_password,
+                hint = "Confirm Password"
+            ) { confirmPassword.value = it }
+
+            Button(
+                onClick = signUpEvent,
+                modifier = Modifier.padding(top = 28.dp, bottom = 8.dp)
+            ) {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = "Register Account"
+                )
+            }
+
+            Row(
+                modifier = Modifier.padding(bottom = 18.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(text = "Already have an Account?")
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                TextButton(onClick = {}) {
+                    Text(text = "Login", color = Blue)
+                }
+
+            }
+
+        }
+    }
 
 }
 
@@ -56,5 +182,5 @@ fun MainTextField(
             .fillMaxWidth(0.9f)
             .padding(top = 12.dp)
     )
-    
+
 }
